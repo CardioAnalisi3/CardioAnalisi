44d849058d93730f11132b1bdc13e303f813b9ed
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2020-02-28 12:49:55 +0100

    Add project files.

---
+++ DataCardio_test/Cardio_test.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DataCardio_test
+{
+    [TestClass]
+    public class Cardio_test
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
