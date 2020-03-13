3fb8af4a4d0ad0cb6608debcf03c6f427b8e939e
Michelangelo <Michelangelo@DESKTOP-C01FU0U>
2020-03-13 19:07:48 +0100

    cambiamenti

--- DataCardio_test/Cardio_test.cs
+++ DataCardio_test/Cardio_test.cs
@@ -6,8 +6,31 @@ namespace DataCardio_test
     public class Cardio_test
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestCalcoloMaxBattitiCardiaci()
         {
+            double risultato =DataCardio.CalcoloMaxBattitiCardiaci(16);
+            Assert.AreEqual(204,risultato);
+        }
+        
+        [TestMethod]
+        public void TestCalcoloMaxBattitiPalestra()
+        {
+            double risultato = DataCardio.CalcoloMaxBattitiPalestra(16);
+            Assert.AreEqual(183.6,risultato);
+        }
+
+        [TestMethod]
+        public void TestCalcoloMinBattitiPalestra()
+        {
+            double risultato = DataCardio.CalcoloMinBattitiPalestra(16);
+            Assert.AreEqual(142.8,risultato);
+        }
+
+        [TestMethod]
+        public void TestBattiti_Riposo()
+        {
+            string risultato = DataCardio.Battiti_Riposo(101);
+            Assert.AreEqual("Tachicardia", risultato);
         }
     }
 }
