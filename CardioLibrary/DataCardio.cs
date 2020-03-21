133be1bb34ab7a7d2c06ab6756d8eb4828e86da3
Michelangelo <Michelangelo@DESKTOP-C01FU0U>
2020-03-21 12:22:37 +0100

    finito

--- CardioLibrary/DataCardio.cs
+++ CardioLibrary/DataCardio.cs
@@ -35,5 +35,78 @@ namespace CardioLibrary
             }
 
         }
+        public double CalorieBruciateUomo(int durata, double peso, double FrequenzaCardiacaMedia, int eta)
+        {
+            double calorie = 0;
+            calorie = (( eta * 0.2017) + (peso * 0.199) + (FrequenzaCardiacaMedia + 0.6309) - 55.0969) * durata / 4.184;
+            calorie = Math.Round(calorie, 2);
+            return calorie;
+        }
+        public double CalorieBruciateDonne (int durata, double peso, double FrequenzaCardiacaMedia, int eta)
+        {
+            double calorie = 0;
+            calorie = ((eta * 0.074) + (peso * 0.126) + (FrequenzaCardiacaMedia + 0.4472) - 20.4022) * durata / 4.184;
+            calorie = Math.Round(calorie, 2);
+            return calorie;
+        }
+        public double SpesaEnergeticaCorsa (int DistanzaPercorsa, int PesoCorporeo)
+        {
+            double SpesaEnergetica = 0.9 * DistanzaPercorsa + PesoCorporeo;
+            SpesaEnergetica = Math.Round(SpesaEnergetica, 2);
+            return SpesaEnergetica;
+        }
+        public double SpesaEnergeticaCamminata (int DistanzaPercorsa, int PesoCorporeo)
+        {
+            double SpesaEnergetica = 0.50 * DistanzaPercorsa + PesoCorporeo;
+            SpesaEnergetica = Math.Round(SpesaEnergetica, 2);
+            return SpesaEnergetica;
+        }
+        public double CalcoloMaxBattitiCamminata (int eta)
+        {
+            double maxbattiti = 0;
+            double maxbattiticamminata = 0; 
+            maxbattiti = 220 - eta;
+            maxbattiticamminata = (59 * maxbattiti) / 100;
+            return maxbattiticamminata;
+        }
+        public double CalcoloMinBattitiCamminata (int eta)
+        {
+            double minbattiti = 0;
+            double minbattiticamminata = 0; 
+            minbattiti = 220 - eta;
+            minbattiticamminata = (50 * maxbattiti) / 100;
+            return minbattiticamminata;
+        }
+        public double CalcoloMaxBattitiAerobica (int eta)
+        {
+            double maxbattiti = 0;
+            double maxbattitiaerobica = 0; 
+            maxbattiti = 220 - eta;
+            maxbattitiaerobica = (79 * maxbattiti) / 100;
+            return maxbattitiaerobica;
+        }
+        public double CalcoloMinBattitiAerobica (int eta)
+        {
+            double minbattiti = 0;
+            double minbattitiaerobica = 0; 
+            minbattiti = 220 - eta;
+            minbattitiaerobica = (70 * minbattiti) / 100;
+            return minbattitiaerobica;
+        }
+        public double CalcoloMediaBattitiAlGiorno (int [] battitiorari)
+        {
+            double media = 0;
+            for (int i = 0; i < battitiorari.Lenght; i++)
+            {
+                media = media + battitiorari[i];
+            }
+            media = media / battitiorari.Lenght;
+            return media;
+        }
+        public double CalcoloMediaBattitiAlRiposo(int[]battitiARiposo)
+        {
+            double media = CalcoloMediaBattitiAlGiorno(battitiARiposo);
+            return media;
+        }
     }
 }
--- DataCardio_test/Cardio_test.cs
+++ DataCardio_test/Cardio_test.cs
@@ -12,7 +12,8 @@ namespace DataCardio_test
             Assert.AreEqual(204,risultato);
         }
         
-        [TestMethod]oMaxBattitiPalestra()
+        [TestMethod]
+        public void TestCalcoloMaxBattitiPalestra()
         {
             double risultato = DataCardio.CalcoloMaxBattitiPalestra(16);
             Assert.AreEqual(183.6, risultato); 
@@ -30,6 +31,52 @@ namespace DataCardio_test
         {
             string risultato = DataCardio.Battiti_Riposo(101);
             Assert.AreEqual("Tachicardia", risultato);
+        }
+        [TestMethod]
+        public void TestCalorieBruciateUomo()
+        {
+            double CalorieBruciate = DataCardio.CalorieBruciateUomo(45, 70, 90, 16);
+            double ris_aspettato= 566.70;
+            Assert.AreEqual(ris_aspettato, CalorieBruciate);
+        }
+        [TestMethod]
+        public void TestCalorieBruciateDonna()
+        {
+            double CalorieBruciate = DataCardio.CalorieBruciateDonna(45, 70, 95, 18);
+            double ris_aspettato = 620.48;
+            Assert.AreEqual(ris_aspettato, CalorieBruciate);
+        }
+        [TestMethod]
+        public void TestSpesaEnergeticaCorsa()
+        {
+            double SpesaEnergetica = DataCardio.SpesaEnergeticaCorsa(5, 70);
+            double ris_aspettato = 74.5;
+            Assert.AreEqual(ris_aspettato, SpesaEnergetica);
+        }
+        [TestMethod]
+        public void TestSpesaEnergeticaCamminata()
+        {
+            double SpesaEnergetica = DataCardio.SpesaEnergeticaCorsa(5, 70);
+            double ris_aspettato = 72.5;
+            Assert.AreEqual(ris_aspettato, SpesaEnergetica);
+        }
+        [TestMethod]
+        public void TestCalcoloMediaBattitiAlGiorno()
+        {
+            int[] battitiorari = new int [4] {43 ,36 ,48 ,32 };
+            double media;
+            media = DataCardio.CalcoloMediaBattitiAlGiorno(battitiorari);
+            double ris_aspettato = 39.75;
+            Assert.AreEqual(ris_aspettato, media);
+        }
+        [TestMethod]
+        public void TestCalcoloBattitiARiposo()
+        {
+            int[] BattitiARiposo = new int [4] {42 ,34 ,45 ,35 };
+            double media;
+            media = DataCardio.CalcoloBattitiARiposo(BattitiARiposo);
+            double ris_aspettato = 39;
+            Assert.AreEqual(ris_aspettato, media);
         }
     }
 }
